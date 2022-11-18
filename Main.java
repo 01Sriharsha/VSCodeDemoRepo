69410507f209547d124d827da31d806cbf3639a3
01Sriharsha <harshavasu463@gmail.com>
2022-11-18 15:26:23 +0530

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("this is demo commit in vscode");
+
+        System.out.println("This is second commit");
     }
 }
